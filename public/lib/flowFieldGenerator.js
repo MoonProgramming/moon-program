a1a8477e8838ab7a9bf5838e3e6ae59eedd672eb
moon.programming <moon.programming@gmail.com>
2022-08-17 21:49:41 +0800

    done project flow field.

--- public/lib/flowFieldGenerator.js
+++ public/lib/flowFieldGenerator.js
@@ -1,240 +1,174 @@
-let sketchHolder = document.getElementById("sketch-holder") || undefined;
 let defaultSize = 400;
-let thresholdSize = defaultSize * 1.075;
+let sketchHolder = document.getElementById("sketch-holder") || undefined;
+let roughnessSlider = document.getElementById("roughnessSlider") || undefined;
+let angleRangeSlider = document.getElementById("angleRangeSlider") || undefined;
+let roughnessOutput = document.getElementById("roughnessOutput") || undefined;
+let angleRangeOutput = document.getElementById("angleRangeOutput") || undefined;
+let downloadBtn = document.getElementById("download") || undefined;
 
-class Particle {
-    constructor() {
-        this.pos = createVector(random(0, width), random(0, height));
+roughnessOutput.innerHTML = roughnessSlider.value;
+angleRangeOutput.innerHTML = angleRangeSlider.value;
+
+roughnessSlider.oninput = function () {
+    roughnessOutput.innerHTML = this.value;
+};
+angleRangeSlider.oninput = function () {
+    angleRangeOutput.innerHTML = this.value;
+};
+
+class flowFieldGenerator {
+
+    constructor(defaultSize, sketchHolder) {
+        this.defaultSize = defaultSize;
+        this.thresholdSize = defaultSize * 1.075;
+        this.sketchHolder = sketchHolder;
     }
 
-    edge() {
-        if (this.pos.x < 0) {
-            this.pos.x = width;
-        }
-        if (this.pos.x > width) {
-            this.pos.x = 0;
-        }
-        if (this.pos.y < 0) {
-            this.pos.y = height;
-        }
-        if (this.pos.y > height) {
-            this.pos.y = 0;
-        }
-    }
+    sketch = (s) => {
+        class Particle {
+            constructor() {
+                this.pos = s.createVector(s.floor(s.random(s.width)), s.floor(s.random(s.height)));
+            }
 
-    show() {
-        stroke(0, 20);
-        point(this.pos.x, this.pos.y);
+            edge() {
+                if (this.pos.x < 0) {
+                    this.pos.x = s.width;
+                }
+                if (this.pos.x > s.width) {
+                    this.pos.x = 0;
+                }
+                if (this.pos.y < 0) {
+                    this.pos.y = s.height;
+                }
+                if (this.pos.y > s.height) {
+                    this.pos.y = 0;
+                }
+            }
+
+            show() {
+                s.stroke(0, 30);
+                s.point(this.pos.x, this.pos.y);
+            }
+        }
+
+        let renderer;
+        let totalSize = 400;
+        let particles = [];
+        let zoff = 0;
+
+        s.preload = () => { }
+        s.setup = () => {
+            renderer = s.createCanvas(this.defaultSize, this.defaultSize);
+
+            let p5Download = new p5.Element(downloadBtn);
+            p5Download.mouseClicked(doDownload);
+
+            // s.pixelDensity(1);
+            s.frameRate(10);
+            s.windowResized();
+        }
+        s.windowResized = () => {
+            let fs = s.fullscreen();
+            if (this.sketchHolder) {
+                if (!fs) {
+                    this.sketchHolder.innerHTML = "";
+                    let maxSize = Math.min(window.visualViewport.width, window.visualViewport.height);
+                    if (maxSize < this.thresholdSize)
+                        totalSize = maxSize * 92 / 100;
+                    else
+                        totalSize = this.defaultSize;
+                    s.resizeCanvas(totalSize, totalSize);
+                    renderer.parent(this.sketchHolder);
+                } else {
+                    totalSize = Math.max(s.windowWidth, s.windowHeight);
+                    s.resizeCanvas(s.windowWidth, s.windowHeight);
+                }
+                start();
+            }
+        }
+        function start() {
+            s.background(250);
+
+            let particleCount = totalSize * 30;
+            particles = [];
+            zoff = 0;
+
+            for (let i = 0; i < particleCount; i++) {
+                let par = new Particle();
+                particles.push(par);
+            }
+            console.log('particle count = ' + particles.length);
+
+            // s.loadPixels();
+            // for (let y = 0; y < s.height; y++) {
+            //     for (let x = 0; x < s.width; x++) {
+            //         let index = (x + y * s.floor(s.width)) * 4;
+            //         s.pixels[index] = 0;
+            //         s.pixels[index + 1] = 0;
+            //         s.pixels[index + 2] = 0;
+            //         s.pixels[index + 3] = 0;
+            //     }
+            // }
+            // s.updatePixels();
+        }
+        s.draw = () => {
+            let roughness = roughnessSlider.value;
+            let angleRange = angleRangeSlider.value;
+            // s.loadPixels();
+            for (let i = 0; i < particles.length; i++) {
+                let par = particles[i];
+                let xoff = par.pos.x * roughness;
+                let yoff = par.pos.y * roughness;
+                par.direction = p5.Vector.fromAngle(s.noise(xoff, yoff, zoff) * s.TWO_PI * angleRange);
+                par.direction.normalize();
+                par.pos.sub(par.direction);
+                par.edge();
+                par.show();
+                // let index = (s.floor(par.pos.x) + s.floor(par.pos.y) * s.floor(s.width)) * 4;
+                // s.pixels[index] = 0;
+                // s.pixels[index + 1] = 0;
+                // s.pixels[index + 2] = 0;
+                // s.pixels[index + 3] += 5;
+            }
+            // s.updatePixels();
+
+
+            zoff += 0.001;
+            console.log(s.frameRate());
+        }
+        s.mouseClicked = () => {
+            if (s.mouseX < s.width && s.mouseY < s.height && s.mouseX > 0 && s.mouseY > 0) {
+                if (s.isLooping()) {
+                    s.noLoop();
+                    console.log('paused');
+                }
+                else {
+                    s.loop();
+                    console.log('resume');
+                }
+            }
+        }
+        function doDownload() {
+            s.saveCanvas(renderer, 'flowField', 'png');
+        }
     }
 }
 
-let particles = [];
-let zoff = 0;
-let paragraph;
-let roughnessSlider;
-let angleRangeSlider;
+let flowFieldInstance = new flowFieldGenerator(defaultSize, sketchHolder);
+let canvas = new p5(flowFieldInstance.sketch, sketchHolder);
 
-function setup() {
-    let canvas = createCanvas(400, 400);
-    canvas.parent('sketch-holder');
-    background(250);
-    for (let i = 0; i < 10000; i++) {
-        let par = new Particle();
-        particles.push(par);
+function generateNew() {
+    if (canvas) {
+        canvas.remove();
+        canvas = null;
     }
-    roughnessSlider = createSlider(0.001, 0.1, 0.01, 0.001);
-    roughnessSlider.parent('attr1-slider');
-    roughnessP = createSpan('Roughness:');
-    roughnessP.parent('attr1-value');
-    angleRangeSlider = createSlider(1, 4, 1, 1);
-    angleRangeSlider.parent('attr2-slider');
-    angleP = createSpan('AngleRange:');
-    angleP.parent('attr2-value');
+    sketchHolder.innerHTML = "";
+    flowFieldInstance = null;
+    flowFieldInstance = new flowFieldGenerator(defaultSize, sketchHolder);
+    canvas = new p5(flowFieldInstance.sketch, sketchHolder);
 }
 
-
-function draw() {
-    let roughness = roughnessSlider.value();
-    let angleRange = angleRangeSlider.value();
-    for (let i = 0; i < particles.length; i++) {
-        let par = particles[i];
-        let xoff = par.pos.x * roughness;
-        let yoff = par.pos.y * roughness;
-        par.direction = p5.Vector.fromAngle(noise(xoff, yoff, zoff) * TWO_PI * angleRange);
-        par.direction.normalize();
-        par.pos.sub(par.direction);
-        par.edge();
-        par.show();
-    }
-    zoff += 0.001;
-
-    roughnessP.html('Roughness: ' + roughness);
-    angleP.html('AngleRange: ' + angleRange);
+function generateFullScreen() {
+    let elem = document.querySelector("canvas");
+    elem.requestFullscreen();
 }
-
-function mouseClicked() {
-    if (mouseX < width && mouseY < height && mouseX > 0 && mouseY > 0) {
-        if (isLooping()) noLoop();
-        else loop();
-    }
-}
-
-// class snakeGameEngine {
-//     defaultSize;
-//     thresholdSize;
-//     sketchHolder;
-//     renderer;
-//     totalSize;
-
-//     x;
-//     y;
-//     mass;
-//     direction;
-//     foodPos;
-//     bodyCount;
-//     grow;
-//     snakeBody;
-
-//     constructor(defaultSize, thresholdSize, sketchHolder) {
-//         this.defaultSize = defaultSize;
-//         this.thresholdSize = thresholdSize;
-//         this.sketchHolder = sketchHolder;
-//     }
-
-//     sketch = (s) => {
-//         s.preload = () => { };
-//         s.setup = () => {
-//             this.renderer = s.createCanvas(this.defaultSize, this.defaultSize);
-//             s.windowResized();
-//             this.start(s);
-//         };
-//         s.windowResized = () => {
-//             if (this.sketchHolder) {
-//                 this.sketchHolder.innerHTML = "";
-//                 let maxSize = Math.min(window.visualViewport.width, window.visualViewport.height);
-//                 if (maxSize < this.thresholdSize)
-//                     this.totalSize = maxSize * 92 / 100;
-//                 else
-//                     this.totalSize = this.defaultSize;
-//                 s.resizeCanvas(this.totalSize, this.totalSize);
-//                 this.renderer.parent(this.sketchHolder);
-//             }
-//         };
-//         s.draw = () => {
-//             s.background(220);
-//             this.updateFood(s);
-//             this.updatePath(s);
-//             this.updateSnake(s);
-
-//             // hit border
-//             if (this.x >= this.totalSize || this.x <= 0 || this.y >= this.totalSize || this.y <= 0) {
-//                 this.direction = 'stop';
-//                 this.gameOver(s, 'Hit Border, Game Over');
-//                 s.noLoop();
-//             }
-//             // hit self
-//             for (let i = this.grow; i < this.bodyCount; i++) {
-//                 let px = this.snakeBody[i][0];
-//                 let py = this.snakeBody[i][1];
-//                 if (this.x == px && this.y == py) {
-//                     this.direction = 'stop';
-//                     this.gameOver(s, 'Hit Self, Game Over');
-//                     s.noLoop();
-//                 }
-//             }
-//         };
-//         s.mouseClicked = () => {
-//             let axis;
-//             if (this.direction === 'left' || this.direction === 'right')
-//                 axis = 'horizontal';
-//             else
-//                 axis = 'vertical';
-//             if (s.mouseX < this.x && axis === 'vertical') {
-//                 this.direction = 'left';
-//             } else if (s.mouseX > this.x && axis === 'vertical') {
-//                 this.direction = 'right';
-//             } else if (s.mouseY < this.y && axis === 'horizontal') {
-//                 this.direction = 'up';
-//             } else if (s.mouseY > this.y && axis === 'horizontal') {
-//                 this.direction = 'down';
-//             }
-//         }
-//         s.keyPressed = () => {
-//             if (s.keyCode === s.LEFT_ARROW && this.direction != 'right') {
-//                 this.direction = 'left';
-//             } else if (s.keyCode === s.RIGHT_ARROW && this.direction != 'left') {
-//                 this.direction = 'right';
-//             } else if (s.keyCode === s.UP_ARROW && this.direction != 'down') {
-//                 this.direction = 'up';
-//             } else if (s.keyCode === s.DOWN_ARROW && this.direction != 'up') {
-//                 this.direction = 'down';
-//             }
-//         }
-//     }
-//     start(s) {
-//         this.x = this.totalSize / 2;
-//         this.y = this.totalSize / 2;
-//         this.mass = this.totalSize / 60;
-//         this.direction = 'stop';
-//         this.bodyCount = 0;
-//         this.grow = s.floor(this.totalSize / 15);
-//         this.snakeBody = [];
-//         this.foodPos = [s.random(1, this.totalSize - this.mass), s.random(1, this.totalSize - this.mass)];
-//     }
-//     updateFood(s) {
-//         let fx = this.foodPos[0];
-//         let fy = this.foodPos[1];
-//         if ((this.x <= fx + this.mass && fx <= this.x + this.mass) && (this.y <= fy + this.mass && fy <= this.y + this.mass)) {
-//             this.bodyCount += this.grow;
-//             for (let i = 0; i < this.grow; i++) {
-//                 this.snakeBody.unshift([this.x, this.y]);
-//             }
-//             this.foodPos = [s.random(1, this.totalSize - this.mass), s.random(1, this.totalSize - this.mass)];
-//         }
-//         s.fill(255);
-//         s.circle(this.foodPos[0], this.foodPos[1], this.mass);
-
-//         s.fill(0, 102, 153, 251);
-//         s.textSize(this.totalSize / 22);
-//         s.textAlign(s.LEFT);
-//         s.text('Food: ' + s.floor(this.bodyCount / this.grow), this.mass, this.totalSize - this.mass);
-//     }
-//     updatePath(s) {
-//         this.snakeBody.unshift([this.x, this.y]);
-//         this.snakeBody.pop();
-
-//         if (this.direction == 'left') this.x -= 1;
-//         else if (this.direction == 'right') this.x += 1;
-//         else if (this.direction == 'up') this.y -= 1;
-//         else if (this.direction == 'down') this.y += 1;
-//     }
-//     updateSnake(s) {
-//         s.fill(0);
-//         s.circle(this.x, this.y, this.mass);
-//         for (let i = 0; i < this.bodyCount; i++) {
-//             let px = this.snakeBody[i][0];
-//             let py = this.snakeBody[i][1];
-//             s.circle(px, py, this.mass);
-//         }
-//     }
-//     gameOver(s, msg) {
-//         s.fill(0, 102, 153, 251);
-//         s.textSize(this.totalSize / 12);
-//         s.textAlign(s.CENTER);
-//         s.text(msg, this.totalSize / 2, this.totalSize / 2);
-//     }
-// }
-
-// let snakeGameInstance = new snakeGameEngine(defaultSize, thresholdSize, sketchHolder);
-// let canvas = new p5(snakeGameInstance.sketch, sketchHolder);
-
-// function generateNew() {
-//     if (canvas) {
-//         canvas.remove();
-//         canvas = null;
-//     }
-//     sketchHolder.innerHTML = "";
-//     snakeGameInstance = null;
-//     snakeGameInstance = new snakeGameEngine(defaultSize, thresholdSize, sketchHolder);
-//     canvas = new p5(snakeGameInstance.sketch, sketchHolder);
-// }
